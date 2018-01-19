320445be399c7099b6723c88a8611c22a0f7d823
SilentDepth <silentdepth@outlook.com>
2018-01-20 04:01:08 +0800

    Style tweaks

--- src/script.js
+++ src/script.js
@@ -1,8 +1,8 @@
 const logo = document.getElementById('logo')
 document.addEventListener('scroll', function () {
-  if (this.documentElement.scrollTop <= 10) {
-    logo.classList.remove('tilt')
-  } else {
-    logo.classList.add('tilt')
-  }
+  if (Math.max(document.documentElement.scrollTop || 0, document.body.scrollTop || 0) <= 10) {
+  logo.classList.remove('tilt')
+} else {
+  logo.classList.add('tilt')
+}
 })
